5be9d3fcd66c8abbdac7de4a444fc7d3d2d31f70
Dhaval Chauhan (DC) <d.chauhan@hotmail.com>
2018-03-09 16:07:30 -0500

    Handle error
    Removed handle error in memcpy operations.

--- addGpu.cu
+++ addGpu.cu
@@ -20,9 +20,9 @@ int main( void ) {
  int a[N], b[N], c[N];
  int *dev_a, *dev_b, *dev_c;
  // allocate the memory on the GPU
- HANDLE_ERROR( cudaMalloc( (void**)&dev_a, N * sizeof(int) ) );
- HANDLE_ERROR( cudaMalloc( (void**)&dev_b, N * sizeof(int) ) );
- HANDLE_ERROR( cudaMalloc( (void**)&dev_c, N * sizeof(int) ) );
+  cudaMalloc( (void**)&dev_a, N * sizeof(int) ) ;
+  cudaMalloc( (void**)&dev_b, N * sizeof(int) ) ;
+  cudaMalloc( (void**)&dev_c, N * sizeof(int) ) ;
  // fill the arrays 'a' and 'b' on the CPU
  for (int i=0; i<N; i++) {
  a[i] = -i;
@@ -30,14 +30,14 @@ int main( void ) {
  }
 
  // copy the arrays 'a' and 'b' to the GPU
- HANDLE_ERROR( cudaMemcpy( dev_a, a, N * sizeof(int),
- cudaMemcpyHostToDevice ) );
- HANDLE_ERROR( cudaMemcpy( dev_b, b, N * sizeof(int),
- cudaMemcpyHostToDevice ) );
+  cudaMemcpy( dev_a, a, N * sizeof(int),
+ cudaMemcpyHostToDevice ) ;
+  cudaMemcpy( dev_b, b, N * sizeof(int),
+ cudaMemcpyHostToDevice ) ;
  add<<<N,1>>>( dev_a, dev_b, dev_c );
  // copy the array 'c' back from the GPU to the CPU
- HANDLE_ERROR( cudaMemcpy( c, dev_c, N * sizeof(int),
- cudaMemcpyDeviceToHost ) );
+  cudaMemcpy( c, dev_c, N * sizeof(int),
+ cudaMemcpyDeviceToHost ) ;
  // display the results
  for (int i=0; i<N; i++) {
  printf( "%d + %d = %d\n", a[i], b[i], c[i] );
