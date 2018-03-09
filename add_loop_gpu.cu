455f9bd5916533562b695ae958729bb90658de65
Dhaval Chauhan (DC) <d.chauhan@hotmail.com>
2018-03-09 16:31:35 -0500

    Update add_loop_gpu.cu

--- add_loop_gpu.cu
+++ add_loop_gpu.cu
@@ -22,7 +22,7 @@ __global__ void add( int *a, int *b, int *c ) {
     int tid = blockIdx.x;    // this thread handles the data at its thread id
     if (tid < N)
         c[tid] = a[tid] + b[tid];
-    printf( " c = %d \n", c[i] );
+    printf( " c = %d \n", c[tid] );
     
 }
 
